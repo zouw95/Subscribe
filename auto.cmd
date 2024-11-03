ee0deedd29a77ecbd49ea66e98aa6d296a53bca5
zouw <13778714244@163.com>
2024-11-04 01:02:32 +0800

    2024年11月04日  1:02:32

--- auto.cmd
+++ auto.cmd
@@ -2,14 +2,16 @@
 
 @title bat 交互执行git命令
 
+C:
+
 cd C:\\Users\\admin\\Desktop\\subscribe
 
-git add .
+git add *.txt *.cmd
 
 git commit -m "%date:~0,4%年%date:~5,2%月%date:~8,2%日 %time:~0,8%"
 
 git push origin master
 
-timeout /t 4
+timeout /t 5
 
 exit
