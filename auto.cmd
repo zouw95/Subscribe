8a69a3d8af415ef5928cea48b91b318aa0b14fd4
zouw <13778714244@163.com>
2024-11-04 21:00:56 +0800

    2024年11月04日 21:00:56

--- auto.cmd
+++ auto.cmd
@@ -2,11 +2,17 @@
 
 @title bat 交互执行git命令
 
-rem E:
+C:
 
-rem cd E:\Users\zouw\Desktop\subscribe
+cd C:\\Users\\admin\\Desktop\\subscribe
 
-git add v2ray*.txt clash*.txt sing*.txt *.cmd
+git add v2ray*.txt
+
+git add clash*.txt
+
+git add sing*.txt
+
+git add *.cmd
 
 git commit -m "%date:~0,4%年%date:~5,2%月%date:~8,2%日 %time:~0,8%"
 
